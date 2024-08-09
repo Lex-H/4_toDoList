5c258588e9d655a6f5c8c71866c90e4240b9edde
Lex-H <ckfly4014737@gmail.com>
2024-08-09 15:37:35 +0800

    li 大致外觀及功能完成

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 // Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+// var myNodelist = document.getElementsByTagName("LI");
+// var i;
+// for (i = 0; i < myNodelist.length; i++) {
+//   var span = document.createElement("SPAN");
+//   var txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   myNodelist[i].appendChild(span);
+// }
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
+// Click on a close button to delete the current list item
+var close = document.getElementsByClassName("delete");
 var i;
 for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
     var div = this.parentElement;
-    div.style.display = "none";
+    div.remove();
   }
 }
 
-// Add a "checked" symbol when clicking on a list item
+// 點擊事件：CheckBox
 var list = document.getElementById("myUL");
 list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
+  // 舊的代碼
+  // if (ev.target.tagName === 'LI') {
+  //   ev.target.classList.toggle('checked');
+  //   console.log("點擊到LI：" );
+  //   console.log(ev.target);
+  // }
+  const checkBoxSrc = "src/img/checkBox.svg";
+  const checkBoxCheckedSrc = "src/img/checkBoxChecked.svg";
+  if (ev.target.getAttribute('class').includes('checkBox')) {
+    // 更改checkBox圖片路徑，以此切換是否打勾
+    if (ev.target.getAttribute('src') === checkBoxSrc) {
+      ev.target.setAttribute('src', checkBoxCheckedSrc)
+    }else{
+      ev.target.setAttribute('src', checkBoxSrc)
+    }
+    // 控制父節點的class，以此改變樣式
+    ev.target.parentElement.classList.toggle('checked');
   }
+  
 }, false);
 
 // Create a new list item when clicking on the "Add" button
