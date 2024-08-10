b3a931584c1a0db8c31d753d7f880c4fdb56fd15
Lex-H <ckfly4014737@gmail.com>
2024-08-10 16:34:56 +0800

    基本完成 新增 刪除 修改todo的功能 / 用copilot debug

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function updateToDoListUL() {
   <ul id="toDoListUL">
         ${HTMLgenerator()}
 
-        <li onclick="addToDo()">
+        <li id="addToDo">
             <img src="src/img/addToDo.svg" class="addToDo">
             <div>
                 代辦事項
@@ -61,7 +61,7 @@ function updateToDoListUL() {
   `;
   document.getElementById("toDoListUL").outerHTML = newHTML;
   // 重新綁訂所有事件，因為用了outerHTML
-  
+  toDoListULEvent();
 }
 
 
@@ -109,119 +109,202 @@ listData.load()
 // 渲染toDoListUL
 updateToDoListUL()
 
-// 點擊事件：li裡面所有的點擊事件
-var list = document.getElementById("toDoListUL");
-list.addEventListener('click', function(ev) {
-  var classClicked =  ev.target.getAttribute('class');
-  if (classClicked === null) {
-    console.log("class=null，這行代碼阻止報錯，沒有問題，請勿刪除")
-    return
-  }
+// 函數：綁定事件=>li裡面所有的點擊事件
+// function liEvent() {
+//   var list = document.getElementById("toDoListUL");
+//   list.addEventListener('click', function(ev) {
+//     var classClicked =  ev.target.getAttribute('class');
+//     if (classClicked === null) {
+//       console.log("class=null，這行代碼阻止報錯，沒有問題，請勿刪除")
+//       return
+//     }
 
-  // 點擊事件：CheckBox
-  const checkBoxSrc = "src/img/checkBox.svg";
-  const checkBoxCheckedSrc = "src/img/checkBoxChecked.svg";
-  if (classClicked.includes('checkBox')) {
-    // 更改checkBox圖片路徑，以此切換是否打勾
-    if (ev.target.getAttribute('src') === checkBoxSrc) {
-      ev.target.setAttribute('src', checkBoxCheckedSrc)
-    }else{
-      ev.target.setAttribute('src', checkBoxSrc)
+//     // 點擊事件：CheckBox
+//     const checkBoxSrc = "src/img/checkBox.svg";
+//     const checkBoxCheckedSrc = "src/img/checkBoxChecked.svg";
+//     if (classClicked.includes('checkBox')) {
+//       // 更改checkBox圖片路徑，以此切換是否打勾
+//       if (ev.target.getAttribute('src') === checkBoxSrc) {
+//         ev.target.setAttribute('src', checkBoxCheckedSrc)
+//       }else{
+//         ev.target.setAttribute('src', checkBoxSrc)
+//       }
+//       // 控制父節點的class，以此改變樣式
+//       ev.target.parentElement.classList.toggle('checked');
+//     }
+
+//     // 點擊事件：toDo
+//     if (classClicked.includes('toDo')) {    
+//       var toDoTextarea = `<textarea class="inputToDoContent">${ev.target.innerText}</textarea>`;
+//       ev.target.outerHTML = toDoTextarea;
+
+//       // 自動儲存內容到變數 toDoSave
+//       var newInput = document.getElementsByClassName("inputToDoContent")[0];
+//       toDoSave = newInput.value; // 使用 value 屬性
+
+//   setTimeout(function() {
+//     var newInput = document.getElementsByClassName("inputToDoContent")[0];
+//     newInput.focus();
+
+//     // 設置光標位置
+//     var clickPosition = ev.clientX - newInput.getBoundingClientRect().left;
+//     var lineHeight = parseInt(window.getComputedStyle(newInput).lineHeight);
+//     var lines = newInput.value.substr(0, newInput.selectionStart).split("\n");
+//     var cursorPosition = lines.length - 1;
+//     var charWidth = newInput.scrollWidth / newInput.cols;
+//     var charPosition = Math.floor(clickPosition / charWidth);
+//     var finalPosition = lines.slice(0, cursorPosition).join("\n").length + charPosition;
+
+//     // 確保光標位置在有效範圍內
+//     finalPosition = Math.min(finalPosition, newInput.value.length);
+//     newInput.setSelectionRange(finalPosition, finalPosition);
+
+//     // 自動調整高度
+//     newInput.style.height = 'auto';
+//     newInput.style.height = newInput.scrollHeight + 'px';
+//   }, 0);
+
+//   // 監聽輸入事件，自動調整高度
+//   document.addEventListener('input', function(event) {
+//     if (event.target.classList.contains('inputToDoContent')) {
+//       event.target.style.height = 'auto';
+//       event.target.style.height = event.target.scrollHeight + 'px';
+
+//       // 自動儲存內容到變數 toDoSave
+//       var newInput = document.getElementsByClassName("inputToDoContent")[0];
+//       toDoSave = newInput.value; // 使用 value 屬性
+//     }
+//   });
+
+//   // 事件：監聽失去焦點，恢復原本內容
+//   document.addEventListener('blur', function(event) {
+//     if (event.target.classList.contains('inputToDoContent')) {
+//       if (event.target.parentNode) { // 確認元素是否有父節點
+//         // 儲存變更到proxyListDataData
+//         var index = event.target.parentElement.getAttribute("id");
+//         proxyListDataData[index].title = toDoSave;
+
+//         // 恢復原本內容
+//         var toDo = `<div class="toDo">
+//                       ${toDoSave}
+//                     </div>`;
+//         event.target.outerHTML = toDo;
+//       }
+//     }
+//   }, true);
+
+//     }
+
+//     // 點擊事件：delete
+//     if (classClicked.includes('delete')) {
+//       // var div = ev.target.parentElement;
+//       // div.remove();
+
+//       index = ev.target.parentElement.getAttribute("id");
+//       proxyListDataData.splice(index, 1);
+//     }
+    
+//   }, false);
+// }
+function liEvent() {
+  var list = document.getElementById("toDoListUL");
+  list.addEventListener('click', function(ev) {
+    var classClicked = ev.target.getAttribute('class');
+    if (classClicked === null) {
+      console.log("class=null，這行代碼阻止報錯，沒有問題，請勿刪除");
+      return;
     }
-    // 控制父節點的class，以此改變樣式
-    ev.target.parentElement.classList.toggle('checked');
-  }
 
-  // 點擊事件：toDo
-  if (classClicked.includes('toDo')) {    
-    var toDoTextarea = `<textarea class="inputToDoContent">${ev.target.innerText}</textarea>`;
-    ev.target.outerHTML = toDoTextarea;
-
-    // 自動儲存內容到變數 toDoSave
-    var newInput = document.getElementsByClassName("inputToDoContent")[0];
-    toDoSave = newInput.value; // 使用 value 屬性
-
-setTimeout(function() {
-  var newInput = document.getElementsByClassName("inputToDoContent")[0];
-  newInput.focus();
-
-  // 設置光標位置
-  var clickPosition = ev.clientX - newInput.getBoundingClientRect().left;
-  var lineHeight = parseInt(window.getComputedStyle(newInput).lineHeight);
-  var lines = newInput.value.substr(0, newInput.selectionStart).split("\n");
-  var cursorPosition = lines.length - 1;
-  var charWidth = newInput.scrollWidth / newInput.cols;
-  var charPosition = Math.floor(clickPosition / charWidth);
-  var finalPosition = lines.slice(0, cursorPosition).join("\n").length + charPosition;
-
-  // 確保光標位置在有效範圍內
-  finalPosition = Math.min(finalPosition, newInput.value.length);
-  newInput.setSelectionRange(finalPosition, finalPosition);
-
-  // 自動調整高度
-  newInput.style.height = 'auto';
-  newInput.style.height = newInput.scrollHeight + 'px';
-}, 0);
-
-// 監聽輸入事件，自動調整高度
-document.addEventListener('input', function(event) {
-  if (event.target.classList.contains('inputToDoContent')) {
-    event.target.style.height = 'auto';
-    event.target.style.height = event.target.scrollHeight + 'px';
-
-    // 自動儲存內容到變數 toDoSave
-    var newInput = document.getElementsByClassName("inputToDoContent")[0];
-    toDoSave = newInput.value; // 使用 value 屬性
-  }
-});
-
-// 監聽失去焦點事件，恢復原本內容
-document.addEventListener('blur', function(event) {
-  if (event.target.classList.contains('inputToDoContent')) {
-    if (event.target.parentNode) { // 確認元素是否有父節點
-      var toDo = `<div class="toDo">
-                    ${toDoSave}
-                  </div>`;
-      event.target.outerHTML = toDo;
-      // toDoSave = "";
+    // 點擊事件：CheckBox
+    const checkBoxSrc = "src/img/checkBox.svg";
+    const checkBoxCheckedSrc = "src/img/checkBoxChecked.svg";
+    if (classClicked.includes('checkBox')) {
+      // 更改checkBox圖片路徑，以此切換是否打勾
+      if (ev.target.getAttribute('src') === checkBoxSrc) {
+        ev.target.setAttribute('src', checkBoxCheckedSrc);
+      } else {
+        ev.target.setAttribute('src', checkBoxSrc);
+      }
+      // 控制父節點的class，以此改變樣式
+      ev.target.parentElement.classList.toggle('checked');
     }
-  }
-}, true);
 
-  }
+    // 點擊事件：toDo
+    if (classClicked.includes('toDo')) {
+      var toDoTextarea = `<textarea class="inputToDoContent">${ev.target.innerText}</textarea>`;
+      ev.target.outerHTML = toDoTextarea;
 
-  // 點擊事件：delete
-  if (classClicked.includes('delete')) {
-    // var div = ev.target.parentElement;
-    // div.remove();
+      var newInput = document.getElementsByClassName("inputToDoContent")[0];
+      var toDoSave = newInput.value; // 使用 value 屬性
 
-    index = ev.target.parentElement.getAttribute("id");
-    proxyListDataData.splice(index, 1);
-  }
-  
-}, false);
+      setTimeout(function() {
+        newInput.focus();
+
+        // 設置光標位置
+        var clickPosition = ev.clientX - newInput.getBoundingClientRect().left;
+        var lineHeight = parseInt(window.getComputedStyle(newInput).lineHeight);
+        var lines = newInput.value.substr(0, newInput.selectionStart).split("\n");
+        var cursorPosition = lines.length - 1;
+        var charWidth = newInput.scrollWidth / newInput.cols;
+        var charPosition = Math.floor(clickPosition / charWidth);
+        var finalPosition = lines.slice(0, cursorPosition).join("\n").length + charPosition;
+
+        // 確保光標位置在有效範圍內
+        finalPosition = Math.min(finalPosition, newInput.value.length);
+        newInput.setSelectionRange(finalPosition, finalPosition);
+
+        // 自動調整高度
+        newInput.style.height = 'auto';
+        newInput.style.height = newInput.scrollHeight + 'px';
+      }, 0);
+
+      // 監聽輸入事件，自動調整高度
+      newInput.addEventListener('input', function(event) {
+        event.target.style.height = 'auto';
+        event.target.style.height = event.target.scrollHeight + 'px';
+
+        // 自動儲存內容到變數 toDoSave
+        toDoSave = event.target.value; // 使用 value 屬性
+      });
+
+      // 事件：監聽失去焦點，恢復原本內容
+      newInput.addEventListener('focusout', function(event) {
+        if (event.target.parentNode) { // 確認元素是否有父節點
+          // 儲存變更到proxyListDataData
+          var index = event.target.parentElement.getAttribute("id");
+          proxyListDataData[index].title = toDoSave;
+
+          // 恢復原本內容
+          var toDo = `<div class="toDo">${toDoSave}</div>`;
+          event.target.outerHTML = toDo;
+        }
+      });
+    }
+
+    // 點擊事件：delete
+    if (classClicked.includes('delete')) {
+      var index = ev.target.parentElement.getAttribute("id");
+      proxyListDataData.splice(index, 1);
+    }
+  }, false);
+}
 
 
-// 新增toDo
-function addToDo() {
-  // var li = document.createElement("li");
-  // var liContent = `
-  //   <li>
-  //           <img src="src/img/handle.png" class="handle">
-  //           <img src="src/img/checkBox.svg" class="checkBox">
-  //           <div class="toDo">
-  //           </div>
-  //           <img src="src/img/delete.svg" class="delete">
-  //   </li>
-  // `;
-  
-  // var list = document.getElementById("toDoListUL");
-  // var referenceNode = list.children[list.children.length - 1]; // 倒數第一個子節點
-  // list.insertBefore(li, referenceNode);
-  // li.outerHTML = liContent;
 
-  proxyListDataData.push({
-    title: "",
-    checked: false,
+
+
+// 函數：綁定事件=>新增toDo
+function addToDoEvet() {
+  document.getElementById("addToDo").addEventListener("click", function() {
+    proxyListDataData.push({
+      title: "",
+      checked: false,
+    });
   });
+}
+
+// 函數：綁定所有toDoListUL裡面的事件
+function toDoListULEvent() {
+  liEvent();
+  addToDoEvet();
 }
